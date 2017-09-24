fb1554b8c593bbaaf9de43c700a4117a346826fd
iAvinashVarma <avinashvarma@live.com>
2017-09-24 21:22:34 +0530

    Updating Google Maps.

--- js/plugins.js
+++ js/plugins.js
@@ -26,89 +26,17 @@
 // Google Map Customization
 // -------------------------------------------------------------
 
-// jQuery(function ($) {
-    
-//     'use strict';
-
-//     var map;
-
-//     map = new GMaps({
-//         el: '#map-wrapper',
-//         lat: 17.492887,
-//         lng: 78.366905,
-//         scrollwheel:false,
-//         zoom: 16,
-//         zoomControl : true,
-//         panControl : true,
-//         streetViewControl : true,
-//         mapTypeControl: false,
-//         overviewMapControl: false,
-//         clickable: true
-//     });
-
-//     var image = '';
-//     map.addMarker({
-//         lat: 17.492887,
-//         lng: 78.366905,
-//         icon: image,
-//         animation: google.maps.Animation.DROP,
-//         verticalAlign: 'bottom',
-//         horizontalAlign: 'center',
-//         backgroundColor: '#d9d9d9',
-//     });
-
-
-//     var styles = [ 
-
-//     {
-//         "featureType": "road",
-//         "stylers": [
-//         { "color": "#ffffff" }
-//         ]
-//     },{
-//         "featureType": "water",
-//         "stylers": [
-//         { "color": "#99b3cc" }
-//         ]
-//     },{
-//         "featureType": "landscape",
-//         "stylers": [
-//         { "color": "#f2efe9" }
-//         ]
-//     },{
-//         "elementType": "labels.text.fill",
-//         "stylers": [
-//         { "color": "#d3cfcf" }
-//         ]
-//     },{
-//         "featureType": "poi",
-//         "stylers": [
-//         { "color": "#bfbfbf" }
-//         ]
-//     },{
-//         "elementType": "labels.text",
-//         "stylers": [
-//         { "saturation": 1 },
-//         { "weight": 0.1 },
-//         { "color": "#000000" }
-//         ]
-//     }
-
-//     ];
-
-//     map.addStyle({
-//         styledMapName:"Styled Map",
-//         styles: styles,
-//         mapTypeId: "map_style"  
-//     });
-
-//     map.setStyle("map_style");
-// }());
-
-/**
- * MAILCHIMP
- */
-// $('#mc-form').ajaxChimp({ 
-//     url: 'http://doelit.us10.list-manage.com/subscribe/post?u=b02e4f21e264536eff4820875&amp;id=4d57faf2cb' 
-//     /* Replace Your AjaxChimp Subscription Link */
-// });
+function initMap() {
+    var matrusriNagar = {
+        lat: 17.492887,
+        lng: 78.366905
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: matrusriNagar
+    });
+    var marker = new google.maps.Marker({
+        position: matrusriNagar,
+        map: map
+    });
+}
